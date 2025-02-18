fefd5fede25f632e7dba772713da8eb89ce586df
fisknils <fishesgotfeelingsto@gmail.com>
2025-02-18 17:40:34 +0100

    add comments

--- examples/download.js
+++ examples/download.js
@@ -1,11 +1,23 @@
 import Indices from '../index-downloader.js';
 
-const indices = new Indices();
+/**
+ * Downloads a specific model and logs the result.
+ * @param {string} model - The name of the model.
+ * @param {string} localPath - The local path to save the downloaded model.
+ */
+const downloadModel = async (model, localPath) => {
+    const indices = new Indices();
 
-indices.download('vosk-model-small-tg-0.22', 'vosk-model-small-tg-0.22.zip').then(result => {
-    if (result.success) {
-        console.log(result.message);
-    } else {
-        console.error(result.message);
+    try {
+        const result = await indices.download(model, localPath);
+        if (result.success) {
+            console.log(result.message);
+        } else {
+            console.error(result.message);
+        }
+    } catch (error) {
+        console.error('Failed to download model:', error.message);
     }
-});
+};
+
+downloadModel('vosk-model-small-tg-0.22', 'vosk-model-small-tg-0.22.zip');
